b1883dfd6356abe3097b7cf98ab6fd2334545003
Howard <chip.shaw4@gmail.com>
2016-08-12 22:06:56 -0600

    Add files via upload

--- portfolio.js
+++ portfolio.js
@@ -1,62 +1,20 @@
-/*function parallax(){
-	$(window).mousemove(function( event ) {
-	  var x = event.pageX / $(window).width();
-	  var y = ( event.pageY - $(window).scrollTop() ) / $(window).height();
 
-	  	$('#parallax-front').css({
-			'transform' : 'translate('+ -x*3.5 +'%, '+ -y*3.5 +'%)'
-		});
-		$('#parallax-front-mid').css({
-			'transform' : 'translate('+ -x*2.25 +'%, '+ -y*2.25 +'%)'
-		});
-		$('#parallax-back-mid').css({
-			'transform' : 'translate('+ -x*1.25 +'%, '+ -y*1.25 +'%)'
-		});
-		$('#parallax').css({
-			'transform' : 'translate('+ -x/1.5 +'%, '+ -y/1.5 +'%)'
-		});	
 
+function menuSetUp(){
+	$('#mobile-menu').click(function(e){
+		e.preventDefault();
+		var nav = $('#top-nav');
+		if( nav.hasClass('open') ){
+			var navHeight = $('#mobile-menu').outerHeight();
+			nav.removeClass('open').height(navHeight);
+		}
+		else{
+			var navHeight = $('#nav-wrap').outerHeight();
+			nav.addClass('open').height(navHeight);
+		}	
 	});
-}*/
+}
 
 $(document).ready(function(){
-	//parallax();
-
-	$('#pa-one').hover(function(){
-		$('#pp-one').toggleClass('pp-show');
-		$('.wrap').toggleClass('pp-hide');
-		$('nav').toggleClass('pp-hide');
-	});
-
-
-	$('a[href*="#"]:not([href="#"])').click(function() {
-		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-		  var target = $(this.hash);
-		  target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-		  if (target.length) {
-		    $('html, body').animate({
-		      scrollTop: target.offset().top
-		    }, 2000);
-		    return false;
-		  }
-		}
-	});
-
-	var scene = document.getElementById('scene');
-	var parallax = new Parallax(scene);
-
-
-
+	menuSetUp();
 });
-
-
-
-
-$(window).load(function() {
-	$("#overlay").fadeOut("slow");
-});
-
-
-/*$(window).scroll(function(){
-	parallax();
-});*/
