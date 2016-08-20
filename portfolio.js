fbbde45dc8c51de68c7e3a8f188c87ec00b4b668
Howard <chip.shaw4@gmail.com>
2016-08-19 22:44:15 -0600

    Add files via upload

--- portfolio.js
+++ portfolio.js
@@ -1,7 +1,17 @@
+var mouseX, mouseY;
+var projectTarget = null;
+var projectTop, projectLeft, projectWidth, projectHeight;
+var drawCanvas = false;
+var homeCtx, hiddenCtx;
+var canvasWidth, canvasHeight;
+var fillChange = 0;
+
+
+
 //OPEN NAV ON HOVER??
 //SET NAV WIDTH USING JS??
 
-function menuSetUp(){
+function menuSetup(){
 	var nav = $('#top-nav');
 	var closeNav = function(){
 		var navHeight = $('#mobile-menu').outerHeight();
@@ -35,10 +45,140 @@ function menuSetUp(){
 	      scrollTop: scrollTo
 	    }, distance * 500);
 	});
+
+	//$('#top-nav').focusin(openNav).focusout(closeNav)
 }
 
 
 
+
+function projectSetup(){
+	var project = $('.project');
+	var hoverHandler = function(e){
+		//var doc = $(document);
+		mouseX = e.pageX;// - doc.scrollLeft();
+		mouseY = e.pageY;// - doc.scrollTop();
+		//console.log(mouseX, mouseY);
+	}
+	project.mouseenter(function(){
+		var that = $(this);
+		projectTarget = that.children('.project-background');
+		projectTop = that.offset().top;
+		projectLeft = that.offset().left;
+		projectHeight = that.height();
+		projectWidth = that.width();
+		//console.log(projectTop, projectLeft, projectHeight, projectWidth)
+	});
+	project.mouseleave(function(){
+		projectTarget = null;
+	});
+	project.mousemove( hoverHandler );
+}
+
+
+
+
+function animateProject(){
+	if( projectTarget ){
+		var originString = 100 * (mouseX - projectLeft) / projectWidth + '% ';
+		originString += 100 * (mouseY - projectTop) / projectHeight + '%';
+		//console.log(originString);
+		projectTarget.css('transform-origin', originString);
+	}
+}
+
+
+function scaleCanvas(){
+	var canvas = $('#canvas-home');
+	canvasHeight = canvas.height();
+	canvasWidth = canvas.width();
+	canvas.attr('height', canvasHeight);
+	canvas.attr('width', canvasWidth);
+	var canvas = $('#canvas-hidden');
+	canvas.attr('height', canvas.height());
+	canvas.attr('width', canvas.width());
+
+	var r = Math.floor(256 * Math.random());
+	var g = Math.floor(256 * Math.random());
+	var b = Math.floor(256 * Math.random()); 
+	homeCtx.fillStyle = 'rgb(' + r + ',' + g +',' + b + ')'
+}
+
+function canvasSetup(){
+	homeCtx = $('#canvas-home')[0].getContext('2d');
+	hiddenCtx = $('#canvas-hidden')[0].getContext('2d');
+	var resizeHandler = function(){
+		scaleCanvas();
+	}
+	$( window ).resize(resizeHandler);
+	scaleCanvas();
+
+
+	var hoverHandler = function(e){
+		//var doc = $(document);
+		mouseX = e.pageX;// - doc.scrollLeft();
+		mouseY = e.pageY;// - doc.scrollTop();
+		//console.log(mouseX, mouseY);
+	}
+
+	var home = $('#home');
+	home.mouseenter(function(){
+		drawCanvas = true;
+	});
+	home.mouseleave(function(){
+		drawCanvas = false;
+	});
+	home.mousedown(function(e){
+		e.preventDefault();
+		fillChange = true;
+		$(document).mouseup(function(){
+			fillChange = false;
+			console.log('off');
+			$(this).off('mouseup');
+
+		});
+	});
+	home.mousemove( hoverHandler );
+
+
+	homeCtx.fillStyle = 'rebeccapurple';
+
+}
+
+function animateCanvas(){
+	var speed = 2;
+	var hidden = $('#canvas-hidden')[0];
+	var canvas = $('#canvas-home')[0];
+	hiddenCtx.clearRect(0,0,canvasWidth,canvasHeight);
+	hiddenCtx.drawImage(canvas, 0, speed, canvasWidth, canvasHeight);
+	homeCtx.clearRect(0,0,canvasWidth,canvasHeight);
+	homeCtx.drawImage(hidden, 0,0,canvasWidth,canvasHeight);
+
+	if( drawCanvas ){
+		if( fillChange ){
+			var r = Math.floor(256 * Math.random());
+			var g = Math.floor(256 * Math.random());
+			var b = Math.floor(256 * Math.random()); 
+			homeCtx.fillStyle = 'rgb(' + r + ',' + g +',' + b + ')'
+		}
+		var x = mouseX + 50*Math.random() - 25;
+		var y = mouseY + 50*Math.random() - 25;
+		homeCtx.fillRect(x - 5, y - 5, 10, 10);
+		//console.log(x, y);
+	}
+}
+
+function animate(){
+	animateProject();
+	animateCanvas();
+	window.requestAnimationFrame(animate);
+}
+
+
 $(document).ready(function(){
-	menuSetUp();
+	menuSetup();
+	projectSetup();
+	canvasSetup();
+
+	animate();
 });
