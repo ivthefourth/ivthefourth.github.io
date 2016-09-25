8e0f025d4966cd75a2adff6d21d5b30297a3b2f9
Howard Shaw <chip.shaw4@gmail.com>
2016-09-25 01:54:25 -0600

    simple weather app done

--- local-weather/weather.js
+++ local-weather/weather.js
@@ -1,55 +1,101 @@
+
+function toC(kelvin){
+	return Math.round(kelvin - 273.15);
+}
+
+function toF(kelvin){
+	return Math.round(kelvin * 9 / 5 - 459.67);
+}
+
+function showDiv(id){
+	var div = document.getElementById(id)
+	div.style.display = 'block';
+	setTimeout(function(){
+		div.style.opacity = '1';
+	}, 500);
+}
+
 function makePage(response, errMsg){
 	if( response ){
-		console.log(response);
-		document.getElementById('content').innerHTML = JSON.stringify(response);
+		var temperature = response.main.temp;
+		var units = 'f'
+		document.getElementById('location').innerHTML = 
+			response.name + ', ' + response.sys.country;
+		document.getElementById('temperature').innerHTML = 
+			toF(temperature) + '°F';
+		document.getElementById('weather').innerHTML = 
+			response.weather[0].description;
+		document.getElementById('icon').innerHTML = 
+			'<img src="http://openweathermap.org/img/w/' + response.weather[0].icon + '.png">';
+		
+		var tempToggle = function(){
+			if( units === 'c'){
+				units = 'f';
+				document.getElementById('temperature').innerHTML = 
+					toF(temperature) + '°F';
+				}
+			else{
+				units = 'c';
+				document.getElementById('temperature').innerHTML = 
+					toC(temperature) + '°C';
+				}
+		}
+		document.getElementById('f-c-toggle').addEventListener(
+			'click', tempToggle, false);
+
+		//document.getElementById('content').innerHTML = JSON.stringify(response);
+		showDiv('wrapper');
 	}
 	else{
 		console.log('boo');
-		document.getElementById('content').innerHTML = errMsg;
+		document.getElementById('err-msg').innerHTML = errMsg;
+		showDiv('err-wrapper');
 	}
 }
 
-function storePosition (pos) {
+function sendWeatherRequest(ispInfo) {
 	var key = '43af6625c5ef2c20e13fd4fe7d20e7df';
-	var coords = pos.coords;
-	var url = 'http://api.openweathermap.org/data/2.5/weather?lat=' +
-			   coords.latitude + '&lon=' + coords.longitude + '&appid=' + key;
-	console.log(url);
+	var url = 'http://api.openweathermap.org/data/2.5/weather?q='+
+				ispInfo.city + ',' + ispInfo.country + '&appid=' + key;
 	var request = new XMLHttpRequest();
 	request.open('GET', url, true);
+	var errMsg = 'There was a problem connecting to the weather service';
 
 	request.onload = function() {
 		if (request.status >= 200 && request.status < 400) {
 			makePage(JSON.parse(request.responseText), null);
 		} else {
-			var errMsg = 'There was a problem connecting to the weather service';
 			makePage(null, errMsg);
 		}
 	};
 
 	request.onerror = function() {
-		var errMsg = 'There was a problem connecting to the weather service';
 		makePage(null, errMsg);
 	};
 
 	request.send();
 }
 
-function geoError(err){
-	var errMsg = err.message;
-	makePage(null, errMsg);
+
+function getLocationFromIP(callback){
+	var request = new XMLHttpRequest();
+	request.open('GET', 'http://ipinfo.io/json', true);
+	var errMsg = 'There was a problem obtaining your location';
+
+	request.onload = function() {
+		if (request.status >= 200 && request.status < 400) {
+			callback(JSON.parse(request.responseText));
+		} else {
+			makePage(null, errMsg);
+		}
+	};
+
+	request.onerror = function() {
+		makePage(null, errMsg);
+	};
+
+	request.send();
 }
 
 
-
-if ("geolocation" in navigator) {
-	/* geolocation is available */
-	navigator.geolocation.getCurrentPosition(storePosition, geoError);
-} else {
-	/* geolocation IS NOT available */
-	var errMsg = 'Your browser or device does not support geolocation.';
-	makePage(null, errMsg);
-}
-
-
-
+getLocationFromIP(sendWeatherRequest);
