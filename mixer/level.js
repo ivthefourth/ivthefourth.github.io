2de34a970b7b864ad90189449c1793583db20a89
Howard Shaw <chip.shaw4@gmail.com>
2017-07-29 18:38:09 -0600

    moved non-project stuff to 'stuff' repository

--- mixer/level.js
+++
@@ -1,9 +0,0 @@
-/*trackHtml = '<div class="mixer-track"></div> '
-
-$(document).ready(function(){
-	waMakeTracks(trackHtml, 15, 'level-tracks');
-});
-*/
-
-
-//DONT forget to scale peak canvas appropriately
